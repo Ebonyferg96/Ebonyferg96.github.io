df6cf2e09cd7821d4ad39ca46d8f85a2b15bbf50
Ebony <Ebonyferg96@gmail.com>
2018-02-12 00:32:44 -0500

    Created New Link/Javascript

---
+++ Interest.js
@@ -0,0 +1,21 @@
+
+function changeColor(color) { 
+	list.style.color = color; 
+}
+	
+function changeFont(){
+          if (arial.selected) {
+               list.style.fontFamily = "arial";
+          }
+          if (times.selected) {
+               list.style.fontFamily = "times";
+          }
+          if(courier.selected) {
+               list.style.fontFamily = "courier";
+          }
+}
+
+
+function myFunction() {
+    alert("A librocubicularist is someone who reads in bed");
+}
